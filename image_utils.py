9ec79b1ff7c96047ecac8b832b19866be640820d
Benjamin <heinen.benjamin@gmail.com>
2019-04-20 20:55:18 +0100

    oublie ?

--- image_utils.py
+++ image_utils.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 import skimage.color as color
-
+import torch
 
 def afficherImageLAB(img_lab):
     plt.imshow(color.lab2rgb(img_lab))
@@ -22,7 +22,7 @@ def show_images(images, titles):
         subplot = f.add_subplot(1,len(images), i)
         # gray
         if (len(images[i-1].shape) == 2):
-            plt.imshow(images[i-1], cmap='gray', vmin=0, vmax=255)
+            plt.imshow(images[i-1], cmap='gray')
         else:
             plt.imshow(images[i-1])
         subplot.set_title(titles[i-1])
@@ -37,12 +37,36 @@ def afficherPrediction(originale, grayscale, prediction):
     subplot.set_title("originale")
 
     subplot = f.add_subplot(1,3, 2)
-    plt.imshow(grayscale.reshape((256,256)), cmap='gray', vmin=0, vmax=255)
+    plt.imshow(grayscale.reshape((256,256)), cmap='gray', vmin=0, vmax=1)
     subplot.set_title("gris")
-    lab = np.concatenate((grayscale, prediction), axis=2)
+    grayscale = grayscale.reshape((256,256,1))
+    img_lab = color.rgb2lab(originale)
+    test = img_lab[:, :, 0]
+    test = test.reshape((256, 256, 1))
+    lab = np.concatenate((test, prediction), axis=2)
     subplot = f.add_subplot(1,3, 3)
+    print('lab2rgb')
     test = color.lab2rgb(lab)
+    print('lab2rgb2')
+
     plt.imshow(color.lab2rgb(lab))
+    print('lab2rgb3')
+
     subplot.set_title("prediction")
 
     plt.show()
+
+
+def to_rgb(grayscale_input, ab_input, save_path=None, save_name=None):
+  '''Show/save rgb image from grayscale and ab channels
+     Input save_path in the form {'grayscale': '/path/', 'colorized': '/path/'}'''
+  plt.clf() # clear matplotlib 
+  color_image = torch.cat((grayscale_input, ab_input), 0).numpy() # combine channels
+  color_image = color_image.transpose((1, 2, 0))  # rescale for matplotlib
+  color_image[:, :, 0:1] = color_image[:, :, 0:1] * 100
+  color_image[:, :, 1:3] = color_image[:, :, 1:3] * 255 - 128   
+  color_image = lab2rgb(color_image.astype(np.float64))
+  grayscale_input = grayscale_input.squeeze().numpy()
+  if save_path is not None and save_name is not None: 
+    plt.imsave(arr=grayscale_input, fname='{}{}'.format(save_path['grayscale'], save_name), cmap='gray')
+    plt.imsave(arr=color_image, fname='{}{}'.format(save_path['colorized'], save_name))
